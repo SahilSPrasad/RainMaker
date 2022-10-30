18fa6b45d4e46a51ecebd06b1b782c2cb4e677f9
SahilSPrasad <SahilSPrasad@github.com>
2022-10-30 00:22:59 -0700

    Added Helicopter and fuel text

--- src/GameApp.java
+++ src/GameApp.java
@@ -3,25 +3,29 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class GameApp extends Application {
 
+
     private final static int GAME_HEIGHT = 800;
     private final static int GAME_WIDTH = 400;
 
-
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
+
 
         Game game = new Game();
         Scene scene = new Scene(game, GAME_WIDTH, GAME_HEIGHT);
-
         setupWindow(game, scene);
 
+
+        scene.setFill(Color.BLACK);
         stage.setScene(scene);
         stage.show();
     }
@@ -32,12 +36,15 @@ public class GameApp extends Application {
     }
 
     void setupWindow(Game game, Scene scene) {
+
         game.setScaleY(-1);
         scene.setFill(Color.BLACK);
     }
 }
 
 class Game extends Pane {
+    int helipadCenterX = 195;
+    int helipadCenterY = 90;
 
     Game() {
         createGameObjects();
@@ -45,10 +52,14 @@ class Game extends Pane {
 
 
     void createGameObjects() {
+        int fuel = 25000;
+
         Pond pond = new Pond();
         Cloud cloud = new Cloud();
-        Helipad helipad = new Helipad();
-        this.getChildren().addAll(pond, cloud, helipad);
+        Helipad helipad = new Helipad(helipadCenterX, helipadCenterY);
+        Helicopter helicopter = new Helicopter(fuel, helipadCenterX,
+                helipadCenterY);
+        this.getChildren().addAll(pond, cloud, helipad, helicopter);
 
     }
 
@@ -78,11 +89,12 @@ class Cloud extends GameObject {
 
 class Helipad extends GameObject {
 
-    Helipad() {
+
+    Helipad(int padCenterX, int padCenterY) {
         Rectangle border = new Rectangle(145, 40, 100, 100);
         border.setStroke(Color.WHITE);
 
-        Circle pad = new Circle(195, 90, 40);
+        Circle pad = new Circle(padCenterX, padCenterY, 40);
         pad.setStroke(Color.WHITE);
 
         this.getChildren().addAll(border, pad);
@@ -92,18 +104,39 @@ class Helipad extends GameObject {
 
 class Helicopter extends GameObject {
 
-    Helicopter() {
-        Circle base = new Circle();
-        Line roter = new Line();
 
+    Helicopter(int fuel, int helipadCenterX, int helipadCenterY) {
+        Circle base = new Circle(helipadCenterX, helipadCenterY, 10,
+                Color.YELLOW);
 
+        Line roter = new Line(195, 90, 195, 120);
+        roter.setStroke(Color.YELLOW);
+        roter.setStrokeWidth(2);
 
+        GameText fuelText = new GameText("F:" + fuel, Color.YELLOW, 175, 75);
+
+        this.getChildren().addAll(base, roter, fuelText);
     }
-
 
 }
 
 interface Updatable {
+
+}
+
+class GameText extends GameObject {
+    Text gameText = new Text();
+
+    GameText(String text, Paint color, int x, int y) {
+
+        gameText.setText(text);
+        gameText.setStroke(color);
+        gameText.setX(x);
+        gameText.setY(y);
+        gameText.setScaleY(-1);
+
+        this.getChildren().add(gameText);
+    }
 
 }
 
